a6d7c2f61e9a2faad69ec0c2c0749acd95697f88
Johannes  BratsethHåkonsen <johannes@tibe.no>
2020-05-29 10:37:14 +0200

    bump version

--- src/Embedder.php
+++ src/Embedder.php
@@ -32,7 +32,7 @@ use yii\base\Event;
 class Embedder extends Plugin
 {
     public static $plugin;
-    public        $schemaVersion = '1.0.1';
+    public        $schemaVersion = '1.0.6';
 
     public function init()
     {
